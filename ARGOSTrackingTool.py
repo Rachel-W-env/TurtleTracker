d5593e27af60574fbd935677e940482f7c75d1f8
Rachel-W-env <rmwilliams100@icloud.com>
2025-09-28 22:39:22 -0400

    Added initial code

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -10,17 +10,17 @@
 #--------------------------------------------------------------
 # Parse Data
 # Copy and paste a line of data as the lineString variable value
-lineString = ""
+lineString = "20640	29051	7/4/2003 3:42	B	0	34.136	-77.725	31.25	-91.766	2	0	-126	53	2	401 651179.0	0"
   
 # Use the split command to parse the items in lineString into a list object
-lineData = lineString
+lineData = lineString.split()
   
 # Assign variables to specfic items in the list
-record_id = lineData[]   # ARGOS tracking record ID
-obs_date = lineData[]   # Observation date
-ob_lc = lineData[]       # Observation Location Class
-obs_lat = lineData[]     # Observation Latitude
-obs_lon = lineData[]     # Observation Longitude
+record_id = lineData[1]   # ARGOS tracking record ID
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[3]       # Observation Location Class
+obs_lat = lineData[5]     # Observation Latitude
+obs_lon = lineData[6]     # Observation Longitude
   
 # Print information to the use
 print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
